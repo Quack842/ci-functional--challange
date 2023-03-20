779656942448f5dd7b6ee92643ea8b76915cb6f0
Nicolene Marais <nici.marais.ie@gmail.com>
2023-03-20 12:23:19 +0000

    Hooks Challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,15 +9,26 @@ export class Content extends Component {
         super(props)
 
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: [],
         }
     }
     componentDidMount() {
         setTimeout(() => {
             this.setState({
                 isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
+    }
+    handleChange = (e) => {
+        const userInput = e.target.value.toLowerCase();
+        const filterPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(userInput);
+        }) 
+        this.setState({
+            posts: filterPosts
+        })
     }
 
     render() {
@@ -25,11 +36,20 @@ export class Content extends Component {
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Posts</h1>
+                    <form>
+                        <label htmlFor='searchInput'>Search: </label>
+                        <input 
+                            type='search' 
+                            id='searchInput'
+                            placeholder='By Author'
+                            onChange={(event) => this.handleChange(event)} />
+                        <h4>Post Found: {this.state.posts.length}</h4>
+                    </form> 
                 </div>
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ? 
-                        <PostItem savedPosts={savedPosts} />
+                        <PostItem savedPosts={this.state.posts} />
                         : <Loader />
                     }
                 </div>
